89c75d04a82752120cad18d8eb10991f16d28ead
yexueliang <yexueliang@jianzhikeji.com>
2022-07-26 15:30:28 +0800

    feat: 修改目录

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,14 @@
 
     <router-view />
     <div class="dev">
-      dev第n次提交
+      /github-actions-demo/
     </div>
-
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: '/gh-page-demo/'
+  publicPath: '/github-actions-demo/'
 }
